b730b00562c77a20b3dd2cbfdc38070f48cfd5b3
adaroflorencia <adaro.florencia@uncuyo.edu.ar>
2025-06-12 12:49:54 -0300

    Se agrega reset de contraseña

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.registro_usuario, name='signup'),
@@ -8,6 +9,18 @@ urlpatterns = [
 
     path('logout/', views.signout, name='logout'),
 
-    path('login/', views.iniciar_sesion, name='login')
+    path('login/', views.iniciar_sesion, name='login'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset_form.html'), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
